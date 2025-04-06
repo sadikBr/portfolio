d15524110b9be021c31712b05999a21b8bc93728
Brahim SADIK <sadbrah@gmail.com>
2025-04-06 19:57:39 +0100

    created the hero section

--- src/components/hero-section/components/threejs-scene.tsx
+++ src/components/hero-section/components/threejs-scene.tsx
@@ -1,7 +1,12 @@
 
+import BrahimImage from '../../../assets/brahim.jpg';
+
 export default function HeroSection3dScene() {
   return (
-    <iframe src='https://my.spline.design/chips-f7c7256f9d337b5a24830255ad24d8b2/' frameBorder='0' width='100%' height='100%'></iframe>
+    <div className='relative w-full h-full'>
+      <img src={BrahimImage} alt='profile image' className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[30%] rounded-full skew-x-12' />
+      <iframe src='https://my.spline.design/chips-f7c7256f9d337b5a24830255ad24d8b2/' width='100%' height='100%'></iframe>
+    </div>
   );
 }
 
--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -2,8 +2,15 @@ import HeroSection3dScene from "./components/threejs-scene"
 
 export default function HeroSection() {
   return (
-    <section className="section relative">
-      <HeroSection3dScene />
+    <section className="flex gap-4 w-screen h-screen">
+      <div className="flex-1">
+        <HeroSection3dScene />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-start">
+        <h1 className="text-5xl leading-loose mb-12">Hi, I am <span className="text-6xl font-bold text-orange-300">Brahim SADIK</span></h1>
+        <p className="text-3xl leading-relaxed pr-4 mb-12">I am a Fullstack Web Developer With a Passion For Creating Beautiful and Interactive Web Applications.</p>
+        <a href="#projects" className="border text-xl border-orange-300 px-12 py-2 rounded-sm font-bold text-orange-300 hover:bg-orange-300 hover:text-blue-950 transition-all hover:transition-all">See My Work</a>
+      </div>
     </section>
   );
 }
