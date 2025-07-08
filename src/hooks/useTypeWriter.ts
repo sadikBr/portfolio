940484d76fde855c8354766a7ba2e0adc1d37a47
SADIK Brahim <sadbrah@gmail.com>
2025-07-08 17:54:56 +0100

    Minor changes

--- src/hooks/useTypeWriter.ts
+++ src/hooks/useTypeWriter.ts
@@ -1,20 +1,26 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo } from "react";
 
 async function sleep(millis: number) {
-  return new Promise(resolve => setTimeout(resolve, millis));
+  return new Promise((resolve) => setTimeout(resolve, millis));
 }
 
 export default function useTypeWriter(typingSpeed: number) {
-  const words = useMemo(() => ['a Developer', 'a Creator', 'Creative', 'a Team Member'], []);
+  const words = useMemo(
+    () => ["a Software Engineer", "Team Driven", "Passionate", "Creative"],
+    [],
+  );
 
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const currentWord = useMemo(() => words[currentWordIndex], [currentWordIndex, words]);
+  const currentWord = useMemo(
+    () => words[currentWordIndex],
+    [currentWordIndex, words],
+  );
 
   const [wordPosition, setWordPosition] = useState(0);
   const displayText = useMemo(() => {
     if (wordPosition === currentWord.length) {
       sleep(500).then(() => {
-        setCurrentWordIndex(prev => (prev + 1) % words.length);
+        setCurrentWordIndex((prev) => (prev + 1) % words.length);
         setWordPosition(0);
         return;
       });
@@ -24,15 +30,15 @@ export default function useTypeWriter(typingSpeed: number) {
 
   useEffect(() => {
     const intervalID = setInterval(() => {
-      setWordPosition(prev => prev + 1);
+      setWordPosition((prev) => prev + 1);
     }, typingSpeed);
 
     return () => {
       clearInterval(intervalID);
-    }
+    };
   }, [typingSpeed]);
 
   return {
-    typedText: displayText
-  }
+    typedText: displayText,
+  };
 }
